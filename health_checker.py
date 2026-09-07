caae85a4a03d68c4f2f46d379fd65091e26e1787
cameron559 <cameronmilsom7@gmail.com>
2026-09-07 14:41:24 +0100

    Add device reachability check

---
+++ health_checker.py
@@ -0,0 +1,13 @@
+import subprocess
+
+def is_reachable(address: str) -> bool:
+    pass
+
+    result = subprocess.run(
+        ["ping", "-n", "1", address],
+        capture_output=True
+    )
+
+    return result.returncode == 0
+
+print(is_reachable("192.168.0.1"))
