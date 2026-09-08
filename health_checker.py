e3d83f8aaa4d544e1d01bcca4950eaaf99caee31
cameron559 <cameronmilsom7@gmail.com>
2026-09-08 15:13:06 +0100

    Fixed formatting

--- health_checker.py
+++ health_checker.py
@@ -1,13 +1,11 @@
 import subprocess
 
-def is_reachable(address: str) -> bool:
-    pass
 
-    result = subprocess.run(
-        ["ping", "-n", "1", address],
-        capture_output=True
-    )
+def is_reachable(address: str) -> bool:
+
+    result = subprocess.run(["ping", "-n", "1", address], capture_output=True)
 
     return result.returncode == 0
+
 
 print(is_reachable("192.168.0.1"))
